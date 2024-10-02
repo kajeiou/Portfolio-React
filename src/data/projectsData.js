c60aa2b876013c69d945895f045459550c76fccc
Nabil Kajeiou <nabilkajeiou2000@gmail.com>
2024-10-02 16:46:23 +0200

    Favicon edited

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,49 +1,49 @@
 const projectsData = [
     {
       title: "WebRTC en React",
-      description: "Application web de diagnostic de compatibilité au WebRTC",
+      description: "Application web de diagnostic de compatibilité au WebRTC. L'outil teste le microphone, la connectivité au STUN et la vitesse d'internet (téléchargement et téléversement)",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/webrtc-react",
       technologies: ["React", "WebRTC", "WebSocket", "Node.js"]
     },
     {
       title: "Panoptes en Angular Et Java Spring Boot",
-      description: "Application web de tableau de bord qui présente des données métriques des clients",
+      description: "Application web de tableau de bord qui présente des données métriques des clients. Les données sont alimentées avec des scripts en Shell executés quotidiennement",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/panoptes-angular-springboot",
-      technologies: ["Angular", "Spring Boot", "Java", "TypeScript","Shell", "PostgreSQL"]
+      technologies: ["Angular", "Spring Boot", "Java", "TypeScript","Shell", "PostgreSQL", "Shell"]
     },
     {
-      title: "MyStats en Python",
-      description: "Application mobile de publication de posts",
+      title: "MyStats avec AWS",
+      description: "Intégration de tableaux de bords Amazon Quicksight par client dans la solution Odigo",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/mystats-python",
       technologies: ["Angular", "AWS", "AWS Lambda Function","AWS Api Gateway", "AWS S3", "AWS Redshift Serverless", "Python"]
     },
     {
       title: "Amigable en React-Native",
-      description: "Application mobile de publication de posts",
+      description: "Application mobile de publication de posts. L'authentification, le stockage des publications et photos se font avec Firebase. Les utilisateurs ont la capacité de publier des posts(textes et images), aimer ou commenter des publications",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/amigable-react-native",
       technologies: ["React-Native", "Firebase", "Firebase Authentification", "Firebase Firestore Database", "Firebase Storage"]
     },
     {
       title: "Flixbus en React-Native",
-      description: "Application mobile de réservation de bus",
+      description: "Application mobile de réservation de bus. L'authentification, le stockage des trajets et photos se font avec Firebase",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/flixbus-react-native",
       technologies: ["React-Native","Google Cloud Platform", "Google Map API","Firebase", "Firebase Authentification", "Firebase Firestore Database", "Firebase Storage"]
     },
     {
       title: "MyLibrary en React et Node",
-      description: "Application web de gestion de livres",
+      description: "Application web de gestion de livres. Il permet d'ajouter, modifier ou supprimer des livres. L'authentification est sécurisé avec un jeton d'authentification JWT",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/mylibrary-react-node",
       technologies: ["React", "Node.js", "MongoDB", "API RESTful"]
     },
     {
       title: "MyCine en Angular et Node",
-      description: "Application de gestion de films",
+      description: "Application de gestion de films. Il permet d'ajouter, modifier ou supprimer des films ou de les noter. L'authentification est sécurisé avec un jeton d'authentification JWT",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/mycine-angular-node",
       technologies: ["Angular", "Node.js", "Express", "MySQL", "API RESTful"]
@@ -64,7 +64,7 @@ const projectsData = [
     },
     {
       title: "KbTravel en PHP",
-      description: "Site internet de réservation de billets d'avion",
+      description: "Site internet de réservation de billets d'avion développé entièrement en PHP Natif. Un espace membre a été développé et une gestion du profil.",
       image: require('../assets/project.jpg'),
       githubLink: "https://github.com/kajeiou/kbtravel-php",
       technologies: ["PHP", "HTML", "CSS", "MySQL"]
